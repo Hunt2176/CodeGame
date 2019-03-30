7dc519b933848836672489045332cf3840ce63ad
Matthew Jackson <mjacks36@outlook.com>
2019-03-30 11:54:51 -0500

    fill out Game instance class

--- app/src/main/java/com/lapis/codefun/com/lapis/backend/GameInstance.java
+++ app/src/main/java/com/lapis/codefun/com/lapis/backend/GameInstance.java
@@ -1,4 +1,47 @@
 package com.lapis.codefun.com.lapis.backend;
 
-public class GameInstance {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GameInstance{
+    private String currentLang;
+    private int numberofQuestions;
+    private Question CurrentCode;
+    private ArrayList<Question> CodeList;
+    private int currentScore;
+    private int currentScoreMulti;
+
+
+    GameInstance(int questionNum, String lang) {
+        currentLang = lang;
+        numberofQuestions = questionNum;
+        CodeList = getQuestionList();
+        currentScore = 0;
+        currentScoreMulti = 1;
+    }
+
+    private ArrayList<Question> getQuestionList() {
+        // get list of questions
+        CodeStore store = new CodeStore();
+        ArrayList<Question> questions = store.getQuestionsByLang(currentLang);
+
+        //shuffle the questions
+        Collections.shuffle(questions);
+
+        // cut off the list of questions
+        questions.subList(numberofQuestions, questions.size() - 1).clear();
+        return questions;
+    }
+
+    public Question getQuestion() {
+        return CodeList.remove(0);
+    }
+
+    public void updateCode() {
+
+    }
+
+
+
+
 }
