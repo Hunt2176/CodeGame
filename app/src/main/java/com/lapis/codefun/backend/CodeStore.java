1545138b3ddcd778371242eb0dfe449b71373164
wyvern <wyvern@uab.edu>
2019-03-30 15:52:03 -0500

    now support 4 languages

--- app/src/main/java/com/lapis/codefun/backend/CodeStore.java
+++ app/src/main/java/com/lapis/codefun/backend/CodeStore.java
@@ -7,10 +7,18 @@ public class CodeStore {
     private HashMap<String, ArrayList<String>> LangList;
     private ArrayList<String> python;
     private ArrayList<String> java;
+    private ArrayList<String> cpp;
+    private ArrayList<String> csharp;
 
     CodeStore(){
         setPythonQuestions();
         setJavaQuestions();
+        setCppQuestions();
+        setCsharpQuestions();
+        LangList.put("Python", python);
+        LangList.put("Java", java);
+        LangList.put("C++", cpp);
+        LangList.put("C#", csharp);
     }
 
     public HashMap<String, ArrayList<String>> getLangList() {
@@ -32,13 +40,13 @@ public class CodeStore {
             "      \"while i > 5:\"\n" +
             "    ],\n" +
             "    [\n" +
-            "      \"\\tprint(i)\"\n" +
+            "      \"\\t\\tprint(i)\"\n" +
             "    ],\n" +
             "    [\n" +
-            "      \"\\ti += 1\",\n" +
-            "      \"\\ti += 2\",\n" +
-            "      \"\\treturn i\",\n" +
-            "      \"\\ti = i - 1\"\n" +
+            "      \"\\t\\ti += 1\",\n" +
+            "      \"\\t\\ti += 2\",\n" +
+            "      \"\\t\\treturn i\",\n" +
+            "      \"\\t\\ti = i - 1\"\n" +
             "    ]\n" +
             "  ],\n" +
             "  \"prompt\": \"Print the numbers 1 through 5\",\n" +
@@ -60,27 +68,27 @@ public class CodeStore {
             "      \"if var = 5:\",\n" +
             "      \"if var == 5:\",\n" +
             "      \"if var = 5+1:\",\n" +
-            "      \"while var = 5\"\n" +
+            "      \"while var = 5:\"\n" +
             "    ],\n" +
             "    [\n" +
-            "      \"\\tprint('var is set to ' + var)\",\n" +
-            "      \"\\tprint('var is set to ', var)\"\n" +
+            "      \"\\t\\tprint('var is set to ' + var)\",\n" +
+            "      \"\\t\\tprint('var is set to ', var)\"\n" +
             "    ],\n" +
             "    [\n" +
             "      \"else:\",\n" +
             "      \"(remove else)\"\n" +
             "    ],\n" +
             "    [\n" +
-            "      \"\\tprint('var is set to something else')\"\n" +
+            "      \"\\t\\tprint('var is set to something else')\"\n" +
             "    ]\n" +
             "  ],\n" +
             "  \"prompt\": \"Set var to 5 and print \\\"var is set to 5\\\"\",\n" +
             "  \"correctCode\": [\n" +
             "    \"var = 5\",\n" +
             "    \"if var == 5:\",\n" +
-            "    \"\\tprint('var is set to ' + var)\",\n" +
+            "    \"\\t\\tprint('var is set to ' + var)\",\n" +
             "    \"else:\",\n" +
-            "    \"\\tprint('var is set to something else')\"\n" +
+            "    \"\\t\\tprint('var is set to something else')\"\n" +
             "  ]\n" +
             "}");
 
@@ -97,24 +105,24 @@ public class CodeStore {
                 "      \"while var = 5\"\n" +
                 "    ],\n" +
                 "    [\n" +
-                "      \"\\tprint('var is set to ' + var)\",\n" +
-                "      \"\\tprint('var is set to ', var)\"\n" +
+                "      \"\\t\\tprint('var is set to ' + var)\",\n" +
+                "      \"\\t\\tprint('var is set to ', var)\"\n" +
                 "    ],\n" +
                 "    [\n" +
                 "      \"else:\",\n" +
                 "      \"(remove else)\"\n" +
                 "    ],\n" +
                 "    [\n" +
-                "      \"\\tprint('var is set to something else')\"\n" +
+                "      \"\\t\\tprint('var is set to something else')\"\n" +
                 "    ]\n" +
                 "  ],\n" +
                 "  \"prompt\": \"Set var to 5 and print \\\"var is set to 5\\\"\",\n" +
                 "  \"correctCode\": [\n" +
                 "    \"var = 5\",\n" +
                 "    \"if var == 5:\",\n" +
-                "    \"\\tprint('var is set to ' + var)\",\n" +
+                "    \"\\t\\tprint('var is set to ' + var)\",\n" +
                 "    \"else:\",\n" +
-                "    \"\\tprint('var is set to something else')\"\n" +
+                "    \"\\t\\tprint('var is set to something else')\"\n" +
                 "  ]\n" +
                 "}");
     }
@@ -134,13 +142,13 @@ public class CodeStore {
                 "      \"while (i > 5){\"\n" +
                 "    ],\n" +
                 "    [\n" +
-                "      \"\\tSystem.out.print(i);\"\n" +
+                "      \"\\t\\tSystem.out.print(i);\"\n" +
                 "    ],\n" +
                 "    [\n" +
-                "      \"\\ti += 1;\",\n" +
-                "      \"\\ti += 2;\",\n" +
-                "      \"\\treturn i;\",\n" +
-                "      \"\\ti = i - 1;\"\n" +
+                "      \"\\t\\ti += 1;\",\n" +
+                "      \"\\t\\ti += 2;\",\n" +
+                "      \"\\t\\treturn i;\",\n" +
+                "      \"\\t\\ti = i - 1;\"\n" +
                 "    ],\n" +
                 "    [\n" +
                 "      \"}\"\n" +
@@ -150,8 +158,8 @@ public class CodeStore {
                 "  \"correctCode\": [\n" +
                 "    \"int i = 1;\",\n" +
                 "    \"while (i <= 5){\",\n" +
-                "    \"\\tSystem.out.print(i);\",\n" +
-                "    \"\\ti += 1;\",\n" +
+                "    \"\\t\\tSystem.out.print(i);\",\n" +
+                "    \"\\t\\ti += 1;\",\n" +
                 "    \"}\"\n" +
                 "  ]\n" +
                 "}");
@@ -184,11 +192,154 @@ public class CodeStore {
                 "  \"correctCode\": [\n" +
                 "    \"int var = 5;\",\n" +
                 "    \"if(var == 5)\",\n" +
-                "    \"\\tSystem.out.print(\\\"var is set to \\\" + var);\",\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to \\\" + var);\",\n" +
                 "    \"else\",\n" +
-                "    \"\\tSystem.out.print(\\\"var is set to something else\\\");\"\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to something else\\\");\"\n" +
                 "  ]\n" +
                 "}");
     }
 
+    private void setCppQuestions(){
+        cpp.add("{\n" +
+                "  \"origCode\": [\n" +
+                "    [\n" +
+                "      \"int i = 0;\",\n" +
+                "      \"int i = 1;\",\n" +
+                "      \"int i = -1;\",\n" +
+                "      \"int i = 2;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"while (i < 5){\",\n" +
+                "      \"while (i <= 5){\",\n" +
+                "      \"while (i > 5){\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"\\t\\tSystem.out.print(i);\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"\\t\\ti += 1;\",\n" +
+                "      \"\\t\\ti += 2;\",\n" +
+                "      \"\\t\\treturn i;\",\n" +
+                "      \"\\t\\ti = i - 1;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"}\"\n" +
+                "    ]\n" +
+                "  ],\n" +
+                "  \"prompt\": \"Print the numbers 1 through 5\",\n" +
+                "  \"correctCode\": [\n" +
+                "    \"int i = 1;\",\n" +
+                "    \"while (i <= 5){\",\n" +
+                "    \"\\t\\tSystem.out.print(i);\",\n" +
+                "    \"\\t\\ti += 1;\",\n" +
+                "    \"}\"\n" +
+                "  ]\n" +
+                "}");
+
+        cpp.add("{\n" +
+                "  \"origCode\": [\n" +
+                "    [\n" +
+                "      \"int var = 5;\",\n" +
+                "      \"int var = 4;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"if(var = 5)\",\n" +
+                "      \"if(var == 5)\",\n" +
+                "      \"if(var = 5+1)\",\n" +
+                "      \"while(var = 5)\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"System.out.print(\\\"var is set to \\\" + var);\",\n" +
+                "      \"System.out.print(\\\"var is set to \\\", var);\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"else\",\n" +
+                "      \"(remove else)\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"System.out.print(\\\"var is set to something else\\\");\"\n" +
+                "    ]\n" +
+                "  ],\n" +
+                "  \"prompt\": \"Set var to 5 and print \\\"var is set to 5\\\"\",\n" +
+                "  \"correctCode\": [\n" +
+                "    \"int var = 5;\",\n" +
+                "    \"if(var == 5)\",\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to \\\" + var);\",\n" +
+                "    \"else\",\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to something else\\\");\"\n" +
+                "  ]\n" +
+                "}");
+    }
+
+    private void setCsharpQuestions(){
+        csharp.add("{\n" +
+                "  \"origCode\": [\n" +
+                "    [\n" +
+                "      \"int i = 0;\",\n" +
+                "      \"int i = 1;\",\n" +
+                "      \"int i = -1;\",\n" +
+                "      \"int i = 2;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"while (i < 5){\",\n" +
+                "      \"while (i <= 5){\",\n" +
+                "      \"while (i > 5){\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"\\t\\tSystem.out.print(i);\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"\\t\\ti += 1;\",\n" +
+                "      \"\\t\\ti += 2;\",\n" +
+                "      \"\\t\\treturn i;\",\n" +
+                "      \"\\t\\ti = i - 1;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"}\"\n" +
+                "    ]\n" +
+                "  ],\n" +
+                "  \"prompt\": \"Print the numbers 1 through 5\",\n" +
+                "  \"correctCode\": [\n" +
+                "    \"int i = 1;\",\n" +
+                "    \"while (i <= 5){\",\n" +
+                "    \"\\t\\tSystem.out.print(i);\",\n" +
+                "    \"\\t\\ti += 1;\",\n" +
+                "    \"}\"\n" +
+                "  ]\n" +
+                "}");
+
+        csharp.add("{\n" +
+                "  \"origCode\": [\n" +
+                "    [\n" +
+                "      \"int var = 5;\",\n" +
+                "      \"int var = 4;\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"if(var = 5)\",\n" +
+                "      \"if(var == 5)\",\n" +
+                "      \"if(var = 5+1)\",\n" +
+                "      \"while(var = 5)\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"System.out.print(\\\"var is set to \\\" + var);\",\n" +
+                "      \"System.out.print(\\\"var is set to \\\", var);\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"else\",\n" +
+                "      \"(remove else)\"\n" +
+                "    ],\n" +
+                "    [\n" +
+                "      \"System.out.print(\\\"var is set to something else\\\");\"\n" +
+                "    ]\n" +
+                "  ],\n" +
+                "  \"prompt\": \"Set var to 5 and print \\\"var is set to 5\\\"\",\n" +
+                "  \"correctCode\": [\n" +
+                "    \"int var = 5;\",\n" +
+                "    \"if(var == 5)\",\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to \\\" + var);\",\n" +
+                "    \"else\",\n" +
+                "    \"\\t\\tSystem.out.print(\\\"var is set to something else\\\");\"\n" +
+                "  ]\n" +
+                "}");
+    }
 }
